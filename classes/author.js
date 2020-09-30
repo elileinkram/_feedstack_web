256e821146ba0ad17f4e0df78f7b05d6ddc8aeb6
Elijah Leinkram <72112684+elijahleinkram@users.noreply.github.com>
2020-09-30 11:26:44 +1000

    Create author.js

---
+++ classes/author.js
@@ -0,0 +1,15 @@
+class Author {
+
+    constructor(authorID, isFollowing, readerID) {
+
+        // type: string
+        // description: a unique id to identify the author
+        this.authorID = authorID;
+
+        // type: boolean
+        // description: checks if the author is following the reader
+        this.isFollowingReader = isFollowing.has(readerID);
+
+    }
+
+}
