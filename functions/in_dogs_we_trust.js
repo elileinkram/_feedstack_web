f27f45ae98faaebe8d886f5ce613d8722e7eb11b
Elijah Leinkram <72112684+elijahleinkram@users.noreply.github.com>
2020-10-03 17:30:02 +1000

    Update in_dogs_we_trust.js

--- functions/in_dogs_we_trust.js
+++ functions/in_dogs_we_trust.js
@@ -8,7 +8,7 @@ module.exports = {
 
         const hashtags = post.hashtags;
 
-        if (hashtags.includes('dog') || hashtags.includes('puppy') || hashtags.includes('puppies')) {
+        if (hashtags.has('dog') || hashtags.has('puppy') || hashtags.has('puppies')) {
 
             const numberOfPeopleThisHasMadeHappy = post.numberOfPeopleThisHasMade.get('happy');
 
